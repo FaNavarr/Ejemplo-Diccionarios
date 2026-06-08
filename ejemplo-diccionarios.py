90f348b98a449cc8c6185b6a5454ea2fbed53a92
FaNavarro <fa.navarrof@duocuc.cl>
2026-06-08 11:55:54 -0400

    modificacion 1

--- ejemplo-diccionarios.py
+++ ejemplo-diccionarios.py
@@ -6,6 +6,12 @@ persona = {
     "lentes": True,
 }
 
+
+print(persona)#mala practica
+print(persona["rut"])
+print(persona["altura"])
+
+#vamos a ver como recorrer un diccionario
 for x in persona:
     print(x," - ",persona[x])
 
@@ -14,13 +20,12 @@ print( persona.keys() ) # muestra las llaves
 print( persona.values() ) # muestra los valores
 print( persona.items() )
 
-print(persona)#mala practica
-print(persona["rut"])
-print(persona["altura"])
+
 
 
 print(persona)
 persona["mascota"]= "Cholito" #si la llave no existe, la agrega
 persona["edad"]= 26 #si la llave existe, modifica el valor!
-persona.pop
+persona.pop("altura") #al indicar la llave, se elimina la llave y el valor
 print(persona)
+print(persona.get)
